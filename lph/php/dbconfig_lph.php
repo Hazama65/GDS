f48f9568d03dfcee1a4f26d1c273601ed4bc977a
Hazama65 <hazamac93@gmail.com>
2023-11-14 11:05:17 -0600

    Configuracion Hostinger

--- lph/php/dbconfig_lph.php
+++ lph/php/dbconfig_lph.php
@@ -1,8 +1,14 @@
 <?php
     // local
-    define('DB_HOST','localhost');
-    define('DB_NAME','lph');
-    define('DB_USERNAME','root');
-    define('DB_PASSWORD','');
+    // define('DB_HOST','localhost');
+    // define('DB_NAME','lph');
+    // define('DB_USERNAME','root');
+    // define('DB_PASSWORD','');
+
+
+    define('DB_HOST','srv961.hstgr.io');
+    define('DB_NAME','u772081794_lph');
+    define('DB_USERNAME','u772081794_lph');
+    define('DB_PASSWORD',':8mU*rK;v');
 
 ?>
