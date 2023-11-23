b9b1edbafd5cccfa41bf1cad397ba19389a39e30
Hazama65 <hazamac93@gmail.com>
2023-11-23 08:38:39 -0600

    Integracion db hostinger

--- hepatitisC/php/dbconfig_hc.php
+++ hepatitisC/php/dbconfig_hc.php
@@ -1,24 +1,34 @@
 <?php
     // local
-    define('DB_HOST','localhost');
-    define('DB_NAME','hepatits');
-    define('DB_USERNAME','root');
-    define('DB_PASSWORD','');
+    // define('DB_HOST','localhost');
+    // define('DB_NAME','hepatits');
+    // define('DB_USERNAME','root');
+    // define('DB_PASSWORD','');
 
-    define('DB_HOST_EM','localhost');
-    define('DB_NAME_EM','bd_estado_municipio');
-    define('DB_USERNAME_EM','root');
-    define('DB_PASSWORD_EM','');
+    // define('DB_HOST_EM','localhost');
+    // define('DB_NAME_EM','bd_estado_municipio');
+    // define('DB_USERNAME_EM','root');
+    // define('DB_PASSWORD_EM','');
 
-    define('DB_HOST_Clues','localhost');
-    define('DB_NAME_Clues','clues');
-    define('DB_USERNAME_Clues','root');
-    define('DB_PASSWORD_Clues','');
+    // define('DB_HOST_Clues','localhost');
+    // define('DB_NAME_Clues','clues');
+    // define('DB_USERNAME_Clues','root');
+    // define('DB_PASSWORD_Clues','');
 
     // hostinger
 
-    // define('DB_HOST','srv961.hstgr.io');
-    // define('DB_NAME','u772081794_interconsulta');
-    // define('DB_USERNAME','u772081794_interconsulta');
-    // define('DB_PASSWORD','d6vo3=R^');
+    define('DB_HOST','srv961.hstgr.io');
+    define('DB_NAME','u772081794_HC');
+    define('DB_USERNAME','u772081794_HC');
+    define('DB_PASSWORD','Z0nt*d1X');
+
+    define('DB_HOST_EM','srv961.hstgr.io');
+    define('DB_NAME_EM','u772081794_EM');
+    define('DB_USERNAME_EM','u772081794_EM');
+    define('DB_PASSWORD_EM','aJ7#P>fDJ|$$');
+
+    define('DB_HOST_Clues','srv961.hstgr.io');
+    define('DB_NAME_Clues','u772081794_clues');
+    define('DB_USERNAME_Clues','u772081794_clues');
+    define('DB_PASSWORD_Clues','m4;#UN[MC');
 ?>
