7b42c7178ef26a48cbe544bd5e5dbc243d0b48d8
Hazama65 <hazamac93@gmail.com>
2023-11-30 11:58:36 -0600

    Cambios hostinger

--- hemofilia/php/dbconfig_hf.php
+++ hemofilia/php/dbconfig_hf.php
@@ -1,25 +1,16 @@
 <?php
     // local
-    define('DB_HOST','localhost');
-    define('DB_NAME','hemofilia');
-    define('DB_USERNAME','root');
-    define('DB_PASSWORD','');
+    // define('DB_HOST','localhost');
+    // define('DB_NAME','hemofilia');
+    // define('DB_USERNAME','root');
+    // define('DB_PASSWORD','');
 
 
     // hostinger
 
-    // define('DB_HOST','srv961.hstgr.io');
-    // define('DB_NAME','u772081794_HC');
-    // define('DB_USERNAME','u772081794_HC');
-    // define('DB_PASSWORD','Z0nt*d1X');
+    define('DB_HOST','srv961.hstgr.io');
+    define('DB_NAME','u772081794_hemofilia');
+    define('DB_USERNAME','u772081794_hemofilia');
+    define('DB_PASSWORD','3r*J:O1UkZ');
 
-    // define('DB_HOST_EM','srv961.hstgr.io');
-    // define('DB_NAME_EM','u772081794_EM');
-    // define('DB_USERNAME_EM','u772081794_EM');
-    // define('DB_PASSWORD_EM','aJ7#P>fDJ|$$');
-
-    // define('DB_HOST_Clues','srv961.hstgr.io');
-    // define('DB_NAME_Clues','u772081794_clues');
-    // define('DB_USERNAME_Clues','u772081794_clues');
-    // define('DB_PASSWORD_Clues','m4;#UN[MC');
 ?>
