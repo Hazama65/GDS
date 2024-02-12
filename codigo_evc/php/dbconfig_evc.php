13e6bc0d0b88d0ae58d5bb7419a31e3dc527f414
Hazama65 <hazamac93@gmail.com>
2024-02-12 15:46:35 -0600

    Hostinger EVC

--- codigo_evc/php/dbconfig_evc.php
+++ codigo_evc/php/dbconfig_evc.php
@@ -1,19 +1,24 @@
 <?php
     // local
-    define('DB_HOST','localhost');
-    define('DB_NAME','evc');
-    define('DB_USERNAME','root');
-    define('DB_PASSWORD','');
+    // define('DB_HOST','localhost');
+    // define('DB_NAME','evc');
+    // define('DB_USERNAME','root');
+    // define('DB_PASSWORD','');
 
-    define('DB_HOS_seg','localhost');
-    define('DB_NAME_seg','evc_seguimiento');
-    define('DB_USERNAME_seg','root');
-    define('DB_PASSWORD_seg','');
+    // define('DB_HOS_seg','localhost');
+    // define('DB_NAME_seg','evc_seguimiento');
+    // define('DB_USERNAME_seg','root');
+    // define('DB_PASSWORD_seg','');
 
     // // hostinger
 
-    // define('DB_HOST','srv961.hstgr.io');
-    // define('DB_NAME','u772081794_Example');
-    // define('DB_USERNAME','u772081794_Example');
-    // define('DB_PASSWORD','Z#7!3I:Pz');
+    define('DB_HOST','srv961.hstgr.io');
+    define('DB_NAME','u772081794_evc');
+    define('DB_USERNAME','u772081794_evc');
+    define('DB_PASSWORD','D6/hiTKE');
+
+    define('DB_HOS_seg','srv961.hstgr.io');
+    define('DB_NAME_seg','u772081794_evc_seg');
+    define('DB_USERNAME_seg','u772081794_evc_seg');
+    define('DB_PASSWORD_seg','S96oz#rs$8');
 ?>
