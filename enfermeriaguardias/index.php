1a852360b2998a1bc7d46a6969100674fe478e3b
Hazama65 <hazamac93@gmail.com>
2024-02-29 15:30:09 -0600

    Registros

--- enfermeriaguardias/index.php
+++ enfermeriaguardias/index.php
@@ -49,9 +49,9 @@ include("modal/registroenfermeriaguardias.php");
             <i class="bi bi-file-earmark-excel"></i> Excel
         </a>
 
-        <a href="graficas_anemia.php">
+        <a href="regitros.php">
             <button type="button" class="btn btn-primary" target="_blank">
-                <i class="bi bi-bar-chart"></i> Gráficas
+                <i class="bi bi-book"></i> Registros
             </button>
         </a>
     </div> <!-- cierre del <div class="btn-group" role="group" aria-label="Basic outlined example">-->
