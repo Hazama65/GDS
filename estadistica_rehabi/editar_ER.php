6cd7795de42859efd81bbe8ac87001446af9866a
DanielJM96 <daniel_juar_mtz96@outlook.es>
2024-05-30 09:04:46 -0600

    Eventos

--- estadistica_rehabi/editar_ER.php
+++ estadistica_rehabi/editar_ER.php
@@ -317,7 +317,7 @@
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-    <script src="js/scripteditar.js"></script>s
+    <script src="js/scripteditar.js"></script>
 
     
     <script type="module">
