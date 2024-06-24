fdaed99621edd6cf5c8cb019c20871dde0a395b7
Hazama65 <hazamac93@gmail.com>
2024-06-24 10:31:33 -0600

    Cambio en graficas

--- personal_enfermeria/metricas.php
+++ personal_enfermeria/metricas.php
@@ -8,7 +8,12 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <link href="css/styles.css" rel="stylesheet" type="text/css" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-
+  <style>
+    #chartdiv {
+      width: 100%;
+      height: 500px;
+    }
+  </style>
 </head>
 
 <body>
@@ -23,24 +28,64 @@
           <div class="edicion-personal">
             <h5 class="modal-title text-center">Datos Actuales</h5>
           </div>
+          <div class="body-container">
+            <div class="titulo-personal">
+              <h6 class="bi bi-person-fill-add"> Resumen Personal por Turno</h6>
+            </div> <br>
+            <div style="overflow-x: auto;">
+              <table class="table">
+                <thead>
+                  <th></th>
+                  <th>JORNADA ESPECIAL</th>
+                  <th>MATUTINO</th>
+                  <th>VESPERTINO</th>
+                  <th>NOCTURNO</th>
+                </thead>
+                <tbody id="tabla-recuento">
+                  <th>Recuento</th>
+                  <td id="recuento-jornada"></td>
+                  <td id="recuento-matutino"></td>
+                  <td id="recuento-vespertino"></td>
+                  <td id="recuento-nocturno"></td>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
+  <div class="container mt-5">
+    <div class="row justify-content-center">
+      <div class="col-md-12">
+        <div class="modal-content">
+          <div class="edicion-personal">
+            <h5 class="modal-title text-center">Gráficas</h5>
+          </div>
+          <div id="chartdiv"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="container mt-5">
+    <div class="row justify-content-center">
+      <div class="col-md-12">
+        <div class="modal-content">
           <div class="body-container">
             <div class="titulo-personal">
               <h6 class="bi bi-person-fill-add"> Resumen Personal por Servicio y Turno</h6>
             </div> <br>
-
-
             <div style="overflow-x: auto;">
               <table class="table">
-                  <thead id="tabla-cabeceras">
-                  </thead>
-                  <tbody id="tabla-datos">
-                  </tbody>
+                <thead id="tabla-cabeceras">
+                </thead>
+                <tbody id="tabla-datos">
+                </tbody>
               </table>
-          </div>
-          
-
-
+            </div>
           </div>
         </div>
       </div>
@@ -51,6 +96,9 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script src="https://cdn.amcharts.com/lib/5/index.js"></script>
+  <script src="https://cdn.amcharts.com/lib/5/xy.js"></script>
+  <script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
   <script src="js/metricas.js"></script>
 
 
--- personal_enfermeria/php/controllers/metricas.controller.php
+++ personal_enfermeria/php/controllers/metricas.controller.php
@@ -17,10 +17,42 @@
     $query_turno = "SELECT * FROM turno";
     $data_turno = $connectionDB->getRows($query_turno);
 
+    $query_jornada = "SELECT COUNT(*) AS recuento_jornada
+        FROM datos_personal dp
+        JOIN turno t ON dp.turno = t.id_turno
+        WHERE t.turno LIKE '%JORNADA ESPECIAL%';";
+    $data_jornada = $connectionDB->getRows($query_jornada);
+
+    $query_matutino = "SELECT COUNT(*) AS recuento_matutino
+        FROM datos_personal dp
+        JOIN turno t ON dp.turno = t.id_turno
+        WHERE t.turno LIKE '%MATUTINO%';";
+    $data_matutino = $connectionDB->getRows($query_matutino);
+
+    $query_vespertino = "SELECT COUNT(*) AS recuento_vespertino
+        FROM datos_personal dp
+        JOIN turno t ON dp.turno = t.id_turno
+        WHERE t.turno LIKE '%VESPERTINO%';";
+    $data_vespertino = $connectionDB->getRows($query_vespertino);
+
+    $query_nocturno = "SELECT COUNT(*) AS recuento_nocturno
+        FROM datos_personal dp
+        JOIN turno t ON dp.turno = t.id_turno
+        WHERE t.turno LIKE '%NOCTURNO%';";
+    $data_nocturno = $connectionDB->getRows($query_nocturno);
+
+    $data_turnos = array(
+        'jornada' => $data_jornada,
+        'matutino' => $data_matutino,
+        'vespertino' => $data_vespertino,
+        'nocturno' => $data_nocturno
+    );
+
     $data = array(
         'personal' => $AllData,
         'servicio' => $data_servicio,
-        'turno' => $data_turno
+        'turno' => $data_turno,
+        'conteo_turnos' => $data_turnos
     );
 
     echo json_encode($data);
